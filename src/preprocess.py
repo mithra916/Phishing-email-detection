0b40283061e37e6241ab8298150b75ff6c11a414
Loga Mithra R <149986612+mithra916@users.noreply.github.com>
2026-01-25 10:40:42 +0530

    Update regex to allow alphanumeric characters in emails

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,6 +4,6 @@ def clean_email(text):
     text = text.lower()
     text = re.sub(r"http\S+", " URL ", text)
     text = re.sub(r"<.*?>", "", text)
-    text = re.sub(r"[^a-zA-Z\s]", " ", text)
+    text = re.sub(r"[^a-zA-Z0-9\s]", " ", text)
     text = re.sub(r"\s+", " ", text)
     return text.strip()
